4a3863bf6356a312ae56cee6178d0483252f9b9a
MD. Asif Joardar <mrsparrow04@gmail.com>
2020-07-27 23:08:39 +0600

    Create Problem B Alchemy.cpp

---
+++ Qualification Round/Problem B Alchemy.cpp
@@ -0,0 +1,56 @@
+/********** BiSmIlLaHiR rAhMaNiR rAhIm ***********\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+\*************** DIIT(17th batch) ****************/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define fs first
+#define ss second
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define PII pair<ll , ll>
+#define SIZE 200000000
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+
+int main()
+{
+    fio();
+    /*#ifndef ONLINE_JUDGE
+    freopen("input2.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif*/
+    ll t; 
+    cin>>t;
+    for(ll l=1;l<=t;l++){
+        ll n,i,a=0,b=0;
+        string s;
+        cin>>n>>s;
+        for(i=0;i<n;i++){
+            if(s[i]=='A') a++;
+            else b++;
+        }
+        if(abs(a-b)==1){
+            cout<<"Case #"<<l<<": Y\n";
+        }
+        else
+            cout<<"Case #"<<l<<": N\n";
+    }
+    return 0;
+}
